bb16739b12d80a5e63b4bcfbe598129c7bc3ca00
Isaac Pereira <135558153+IsaacNw321@users.noreply.github.com>
2025-01-20 17:31:09 -0400

    fix: config

--- server/config.js
+++ server/config.js
@@ -13,6 +13,9 @@ export const SERVER_PORT = process.env.SERVER_PORT || '3000';
 
 export const DATABASE_URL = process.env.DATABASE_URL || `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = DATABASE_URL;
+}
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 
 export const PRISMA_LOG_LEVEL = process.env.PRISMA_LOG_LEVEL || 'info';
